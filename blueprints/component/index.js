0f9d166d4d67a134278ffc813e7e8cf61d3db635
Marty Dill <marty.dill@adroll.com>
2015-12-18 15:07:43 -0800

    Fixed bug in the component template. We really need a better way of handling whitespace.

--- blueprints/component/index.js
+++ blueprints/component/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
         if(args.length) {
             propTypes = "__name__.propTypes = {";
-            defaultProps = "\n    getDefaultProps: function() {";
+            defaultProps = "\n    getDefaultProps() {\n        return {";
 
             for(let index in args) {
                 let prop = args[index];
@@ -18,11 +18,11 @@ module.exports = {
                     throw new Error(`Prop ${prop} is formatted incorrectly`);
                 }
                 propTypes += `\n    ${parts[0]}: ${this.reactPropTypeFrom(parts[1])},`;
-                defaultProps += `\n        ${parts[0]}: ${this.reactDefaultPropFrom(parts[1])},`;
+                defaultProps += `\n            ${parts[0]}: ${this.reactDefaultPropFrom(parts[1])},`;
             }
 
             propTypes += "\n}\n";
-            defaultProps += "\n    },\n";
+            defaultProps += "\n        }\n    }\n";
         }
 
         return {'__proptypes__': propTypes, '__defaultprops__': defaultProps };

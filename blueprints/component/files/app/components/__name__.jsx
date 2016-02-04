c439e20dce0c3a259cc8a64c59215d73dadbcf8a
Justin Niessner <CrshOverride@gmail.com>
2016-02-04 14:42:31 -0800

    Add missed blueprint file

--- blueprints/component/files/app/components/__name__.jsx
+++ blueprints/component/files/app/components/__name__.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 class __name__ extends React.Component {
-    __defaultprops__
-    render() {
-        return (
-            <div/>
-        );
-    }
+  __defaultprops__
+  render() {
+    return (
+      <div/>
+    );
+  }
 }
 
 __proptypes__
